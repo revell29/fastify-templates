fc22d73338f9a50480a4757bd5bff5c397dff170
apsya Dira <diraapsya0@gmail.com>
2022-08-02 16:55:13 +0700

    chore: ignore node_modules and dist

--- dist/controllers/news.controller.js
+++
@@ -1,2 +0,0 @@
-"use strict";Object.defineProperty(exports,"__esModule",{value:!0}),Object.defineProperty(exports,"default",{enumerable:!0,get:()=>a});const a=class{static async create(a,b){try{return b.code(200).send({message:"Hellow"})}catch(c){return b.code(500).send({message:"hellow"})}}}
-//# sourceMappingURL=news.controller.js.map
